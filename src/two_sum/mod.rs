25b129771b3a3e5946606c842a729285cd6f45c1
Caitlin Gao <caitlin.gao@strikingly.com>
2019-03-23 12:32:39 +0800

    feat(leetcode-for-rust): refactor document show

--- src/two_sum/mod.rs
+++ src/two_sum/mod.rs
@@ -12,13 +12,14 @@
 //!
 //! return `[0, 1]`.
 
+use std::collections::HashMap;
 /// # Solutions
 ///
-/// Approach 1: One-pass Hash Table
+/// # Approach 1: One-pass Hash Table
 ///
-/// * Time complexity : O(n)O(n). We traverse the list containing nn elements only once. Each look up in the table costs only O(1)O(1) time.
+/// * Time complexity: O(n)
 ///
-/// * Space complexity : O(n)O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nn elements.
+/// * Space complexity: O(n)
 ///
 /// ```rust
 /// use std::collections::HashMap;
@@ -43,11 +44,11 @@
 /// }
 /// ```
 ///
-/// Approach 1: Brute Force
+/// # Approach 2: Brute Force
 ///
-/// Time complexity : O(n^2)O(n 2). For each element, we try to find its complement by looping through the rest of array which takes O(n)O(n) time. Therefore, the time complexity is O(n^2)O(n 2).
+/// * Time complexity: O(n^2)
 ///
-/// Space complexity : O(1)O(1).
+/// * Space complexity: O(1)
 ///
 /// ```rust
 /// impl Solution {
@@ -73,4 +74,18 @@
 /// }
 ///
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    if nums.len() < 2 {
+        return vec![];
+    }
+
+    let mut positon = HashMap::new();
+    for(i, num) in nums.iter().enumerate() {
+        if positon.contains_key(num) {
+            return vec![positon[num] as i32, i as i32];
+        } else {
+            positon.insert(target - num, i);
+        }
+    }
+
+    return vec![];
 }
