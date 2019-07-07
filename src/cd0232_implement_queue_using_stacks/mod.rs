df5ec89de39fab910d97e499da837735faeef990
Caitlin Gao <caitlin.gao@strikingly.com>
2019-07-07 09:48:52 +0800

    feat(leetcode_rust): add new solution for 323 implement queue using stacks

--- src/cd0232_implement_queue_using_stacks/mod.rs
+++ src/cd0232_implement_queue_using_stacks/mod.rs
@@ -84,4 +84,62 @@
 ///  * let ret_4: bool = obj.empty();
 ///  */
 /// ```
+///
+/// # Approach 2: Stack
+///
+/// * Time complexity: O(n)
+///
+/// * Space complexity: O(n)
+///
+/// ```rust
+/// #[derive(Default)]
+/// struct MyQueue {
+///     stack: Vec<i32>,
+///     queue: Vec<i32>,
+/// }
+///
+/// /**
+///  * `&self` means the method takes an immutable reference.
+///  * If you need a mutable reference, change it to `&mut self` instead.
+///  */
+/// impl MyQueue {
+///
+///     /** Initialize your data structure here. */
+///     fn new() -> Self {
+///         Default::default()
+///     }
+///
+///     /** Push element x to the back of queue. */
+///     fn push(&mut self, x: i32) {
+///         while let Some(q) = self.queue.pop() { self.stack.push(q); }
+///         self.stack.push(x);
+///         while let Some(s) = self.stack.pop() { self.queue.push(s); }
+///     }
+///
+///     /** Removes the element from in front of queue and returns that element. */
+///     fn pop(&mut self) -> i32 {
+///         self.queue.pop().unwrap()
+///     }
+///
+///     /** Get the front element. */
+///     fn peek(&self) -> i32 {
+///         *self.queue.last().unwrap()
+///     }
+///
+///     /** Returns whether the queue is empty. */
+///     fn empty(&self) -> bool {
+///         self.queue.is_empty()
+///     }
+/// }
+///
+/// /**
+///  * Your MyQueue object will be instantiated and called as such:
+///  * let obj = MyQueue::new();
+///  * obj.push(x);
+///  * let ret_2: i32 = obj.pop();
+///  * let ret_3: i32 = obj.peek();
+///  * let ret_4: bool = obj.empty();
+///  */
+/// ```
+///
 pub struct MyQueue;
